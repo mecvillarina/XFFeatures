5223df84d76a3274f50bd18f4b214dc7d939a46e
Mark Erwin C. Villarina <me.villarina@live.com>
2019-12-11 20:56:57 +0800

    CollectionView

--- XFFeatures/XFFeatures/ViewModels/CollectionViewPageViewModel.cs
+++ XFFeatures/XFFeatures/ViewModels/CollectionViewPageViewModel.cs
@@ -28,16 +28,16 @@ namespace XFFeatures.ViewModels
             base.OnNavigatedTo(parameters);
 
             Users = new ObservableCollection<PersonItemModel>();
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Red });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Blue });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Green });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Yellow });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Pink });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.AliceBlue });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Bisque});
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Black });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Cyan });
-            Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Crimson });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Red });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Blue });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Green });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Yellow });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Pink });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.AliceBlue });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Bisque});
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Black });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Cyan });
+            //Users.Add(new PersonItemModel() { FirstName = "Juan", LastName = "Dela Cruz", PersonColor = Color.Crimson });
         }
     }
 }
